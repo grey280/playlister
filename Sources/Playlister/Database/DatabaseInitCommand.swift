f956589d79cf81898256488ee01fd7e5e1a0324f
grey280 <greypatterson@gmail.com>
2020-02-16 11:03:48 -0800

    Option to delete, if wanted

--- Sources/Playlister/Database/DatabaseInitCommand.swift
+++ Sources/Playlister/Database/DatabaseInitCommand.swift
@@ -14,8 +14,16 @@ class DatabaseInitCommand: Command {
     let name = "init"
     let shortDescription: String = "Initialize the database"
     
+    @Flag("-h", "--hard", description: "Delete the existing database") var hard: Bool
+    
     func execute() throws {
         let workDir = try Folder.home.createSubfolderIfNeeded(at: ".playlister")
+        if (hard) {
+            if let oldDB = try? workDir.file(at: "links.sqlite3"){
+                try oldDB.delete()
+            }
+        }
+        
         let db = try Connection("\(workDir.path)/links.sqlite3")
         let links = Table("links")
         let id = Expression<Int64>("id")
