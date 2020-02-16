8e6f460b2dd564b96516d8e794bda1af79038ad0
grey280 <greypatterson@gmail.com>
2020-02-15 17:50:03 -0800

    nope you need to add it as a dependency there too

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Playlister",
-            dependencies: []),
+            dependencies: ["SwiftCLI"]),
         .testTarget(
             name: "PlaylisterTests",
             dependencies: ["Playlister"]),
