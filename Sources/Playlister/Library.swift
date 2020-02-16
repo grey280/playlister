8fe31f4bff60a26e41d89235df7fbefaa6f7a650
grey280 <greypatterson@gmail.com>
2020-02-15 18:24:39 -0800

    initialize library

--- Sources/Playlister/Library.swift
+++ Sources/Playlister/Library.swift
@@ -15,5 +15,21 @@ struct Library {
     init() throws {
         library = try ITLibrary(apiVersion: "1.0")
         playlists = []
+        
+        var queue = library.allPlaylists.filter { (playlist) -> Bool in
+            !playlist.isMaster && playlist.isVisible && playlist.distinguishedKind == .kindNone
+        }
+        while (queue.count > 0){
+            let playlist = queue.remove(at: 0)
+            if let parentID = playlist.parentID{
+                if let parent = playlists.compactMap({$0.findParent(parentID)}).first{
+                    parent.children.append(Playlist(playlist))
+                } else {
+                    queue.append(playlist)
+                }
+            } else {
+                playlists.append(Playlist(playlist))
+            }
+        }
     }
 }
