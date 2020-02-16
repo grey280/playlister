ed17f113eb121f00ff6fcef2cf5e6d35dcc68044
grey280 <greypatterson@gmail.com>
2020-02-16 10:54:17 -0800

    stub init

--- Sources/Playlister/Database/DatabaseInitCommand.swift
+++ Sources/Playlister/Database/DatabaseInitCommand.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import SwiftCLI
+import Files
+
+class DatabaseInitCommand: Command {
+    let name = "init"
+    let shortDescription: String = "Initialize the database"
+    
+    func execute() throws {
+        let workDir = try Folder.home.createSubfolderIfNeeded(at: ".playlister")
+        // TODO: Create the SQLite database in that directory
+    }
+}
