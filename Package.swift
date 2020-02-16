ade938ff62ec52ce89422f6ec7f841fa1105b72b
grey280 <greypatterson@gmail.com>
2020-02-15 19:26:28 -0800

    nevermind put it back

--- Package.swift
+++ Package.swift
@@ -15,11 +15,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "LibPlaylister"
-            ),
-        .target(
             name: "Playlister",
-            dependencies: ["LibPlaylister", "SwiftCLI"]),
+            dependencies: ["SwiftCLI"],
         .testTarget(
             name: "PlaylisterTests",
             dependencies: ["Playlister"]),
