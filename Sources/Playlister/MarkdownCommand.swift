54b43e95cba114ec41be335d5d2e1a2af64e7388
grey280 <greypatterson@gmail.com>
2020-02-15 17:57:03 -0800

    Markdown command

---
+++ Sources/Playlister/MarkdownCommand.swift
@@ -0,0 +1,10 @@
+import SwiftCLI
+
+class MarkdownCommand: Command {
+    let name = "md"
+    let shortDescription = "Generate Markdown files of playlists."
+    
+    func execute() throws {
+        // TODO: Implement
+    }
+}
