c4ccae028993cf79a661352e0a60c318072c7d38
grey280 <greypatterson@gmail.com>
2020-02-15 18:22:08 -0800

    We can throw!

--- Sources/Playlister/Library.swift
+++ Sources/Playlister/Library.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import iTunesLibrary
+
+struct Library {
+    var playlists: [Playlist]
+    private let library: ITLibrary
+    
+    init() throws {
+        library = try ITLibrary(apiVersion: "1.0")
+        playlists = []
+    }
+}
