b8877c3c2476716b8ab1f389a1ea6dacd156a3e3
grey280 <greypatterson@gmail.com>
2020-02-15 18:02:50 -0800

    bring in stuff from my original one

--- Sources/Playlister/Playlist.swift
+++ Sources/Playlister/Playlist.swift
@@ -8,4 +8,37 @@
 import Foundation
 import iTunesLibrary
 
-
+struct Playlist: Identifiable {
+    typealias ID = NSNumber
+    
+    private let origin: ITLibPlaylist
+    var children: [Playlist] = []
+    var id: ID{
+        origin.persistentID
+    }
+    
+    var parentID: Playlist.ID?
+    
+    var name: String{
+        origin.name
+    }
+    
+    var isParent: Bool{
+        children.count > 0
+    }
+    
+    init(_ from: ITLibPlaylist){
+        origin = from
+    }
+    
+    func findParent(_ of: Playlist.ID) -> Playlist?{
+        if (id == of){
+            return self
+        }
+        return children.compactMap({ $0.findParent(of)}).first
+    }
+    
+    func findParent(_ of: ITLibPlaylist) -> Playlist? {
+        findParent(of.persistentID)
+    }
+}
