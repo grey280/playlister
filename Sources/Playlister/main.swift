95c11cd830f371e57ae35ca73d8742548b062123
grey280 <greypatterson@gmail.com>
2020-02-15 18:41:42 -0800

    and… run?

--- Sources/Playlister/main.swift
+++ Sources/Playlister/main.swift
@@ -1,3 +1,4 @@
 import SwiftCLI
 
-
+let cli = CLI(name: "playlister", version: "2.0.0", description: "A tool for tracking your iTunes/Music.app playlists as they change over time.", commands: [MarkdownCommand()])
+let _ = cli.go()
