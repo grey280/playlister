0c65d4f1cfb78c5b90ed9a491a1b44e1ad2a420d
grey280 <greypatterson@gmail.com>
2020-02-18 11:34:34 -0800

    forgot to include it in the cli

--- Sources/Playlister/main.swift
+++ Sources/Playlister/main.swift
@@ -1,4 +1,4 @@
 import SwiftCLI
 
-let cli = CLI(name: "playlister", version: "2.0.0", description: "A tool for tracking your iTunes/Music.app playlists as they change over time.", commands: [MarkdownCommand(), ListCommand(), DatabaseCommandGroup()])
+let cli = CLI(name: "playlister", version: "2.0.0", description: "A tool for tracking your iTunes/Music.app playlists as they change over time.", commands: [MarkdownCommand(), ListCommand(), GenerateCommand(), DatabaseCommandGroup()])
 let _ = cli.go()
