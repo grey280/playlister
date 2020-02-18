d942b3bfba9ecba3259166c3db1a9070280d4717
grey280 <greypatterson@gmail.com>
2020-02-18 11:22:14 -0800

    let's have another command

---
+++ Sources/Playlister/GenerateCommand.swift
@@ -0,0 +1,17 @@
+import SwiftCLI
+import Foundation
+import Files
+
+class GenerateCommand : Command {
+    
+    
+    
+    
+    let name = "generate"
+    let shortDescription = "Generate all playlists as markdown"
+    let longDescription = "Output all playlists as markdown files in their full hierarchical structure"
+    
+    func execute() throws {
+        <#code#>
+    }
+}
