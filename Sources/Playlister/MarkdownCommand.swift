867dabe733a8f3bf0d410d1984f11004ee8c3cf4
grey280 <greypatterson@gmail.com>
2020-02-21 17:48:23 -0800

    we can clean up a lot now

--- Sources/Playlister/MarkdownCommand.swift
+++ Sources/Playlister/MarkdownCommand.swift
@@ -64,44 +64,6 @@ class MarkdownCommand: Command {
     }
 }
 
-/*
-class MarkdownCommand: Command {
-    let name = "md"
-    let shortDescription = "Generate Markdown files of playlists."
-    
-    // Base output path to use when generating
-    @Param var path: String
-    
-    @Flag("-r", "--rating", description: "Include star ratings in output") var rating: Bool
-    
-    @Key("-m", "--message", description: "Message to use when committing to Git repository.") var gitMessage: String?
-    
-    func execute() throws {
-        let library = try Library()
-        let filemanager = FileManager.default
-//        let inp = PipeStream()
-//        let gitInit = Task(executable: "git", arguments: ["init"], directory: path, stdout: stdout, stderr: stderr, stdin: inp)
-//        gitInit.runSync()
-//        let gitClear = Task(executable: "git", arguments: ["rm", "-rf", "."], directory: path, stdout: stdout, stderr: stderr, stdin: inp)
-//        gitClear.runSync()
-//        let gitClean = Task(executable: "git", arguments: ["clean", "-fxd"], directory: path, stdout: stdout, stderr: stderr, stdin: inp)
-//        gitClean.runSync()
-        
-        for playlist in library.playlists {
-            playlist.printPlaylist(in: path, with: filemanager)
-        }
-        
-//        let gitAdd = Task(executable: "git", arguments: ["add", "."], directory: path, stdout: stdout, stderr: stderr, stdin: inp)
-//        gitAdd.runSync()
-//        let gitCommit = Task(executable: "git", arguments: ["commit", "-m", gitMessage ?? "Automated update"], directory: path, stdout: stdout, stderr: stderr, stdin: inp)
-//        gitCommit.runSync()
-//        let gitPush = Task(executable: "git", arguments: ["push"], directory: path, stdout: stdout, stderr: stderr, stdin: inp)
-//        gitPush.runSync()
-    }
-}
-*/
-
-
 internal extension Playlist {
     /// Convert a playlist to markdown
     /// - Parameters:
@@ -147,46 +109,3 @@ internal extension Playlist {
         return "# \(name)\n\n\(body)\n"
     }
 }
-    /*
-    func printPlaylist(in path: String, with filemanager: FileManager, includeRating: Bool = false){
-        if isParent{
-            // create directory and recurse
-            let newPath = "\(path)/\(name)"
-            if (!filemanager.fileExists(atPath: newPath)){
-                do {
-                    try filemanager.createDirectory(atPath: newPath, withIntermediateDirectories: false, attributes: nil)
-                } catch {
-                    FileHandle.standardError.write("Error: failed to create directory \(newPath)".data(using: .utf8)!)
-                    exit(1)
-                }
-            }
-            for child in children{
-                child.printPlaylist(in: newPath, with: filemanager)
-            }
-        } else {
-            // print the playlist!
-            let newFileName = "\(path)/\(name).md"
-            if (filemanager.fileExists(atPath: newFileName)){
-                print("Removing file \(newFileName) for overwriting")
-                do{
-                    try filemanager.removeItem(atPath: newFileName)
-                }catch let error{
-                    FileHandle.standardError.write("Error: failed to remove file \(newFileName)".data(using: .utf8)!);
-                    FileHandle.standardError.write(error.localizedDescription.data(using: .utf8)!)
-                    exit(1)
-                }
-            }
-            // build the file contents
-            guard let body = asMarkdown(includeRating: includeRating) else {
-                FileHandle.standardError.write("Error: failed to build playlist descriptor \(newFileName)".data(using: .utf8)!);
-                exit(1)
-            }
-            //   write to the file
-            guard filemanager.createFile(atPath: newFileName, contents: body.data(using: .utf8)!, attributes: nil) else{
-                FileHandle.standardError.write("Error: failed to write file \(newFileName)".data(using: .utf8)!);
-                exit(1)
-            }
-        }
-    }
-}
-*/
