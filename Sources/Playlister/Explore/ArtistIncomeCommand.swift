a24266a59e7f242ebe93e79fe9ca7d574373ea0c
grey280 <greypatterson@gmail.com>
2020-02-29 15:51:15 -0800

    what it is

--- Sources/Playlister/Explore/ArtistIncomeCommand.swift
+++ Sources/Playlister/Explore/ArtistIncomeCommand.swift
@@ -9,3 +9,13 @@ import SwiftCLI
 import Foundation
 
 // TODO: This should take an artist name, then get all their songs, and then total up the counts across all of those, and finally multiply it by Apple Music's 'payout per artist' thing
+
+class ArtistIncomeCommand: Command {
+    func execute() throws {
+        <#code#>
+    }
+    
+    let name = "income"
+    
+    let shortDescription: String = "Estimate the amount of money an artist has made from you listening to their music."
+}
