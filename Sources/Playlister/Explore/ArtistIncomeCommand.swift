f58802c5e93b0acdbe79dd78fc227e4afabc6797
grey280 <greypatterson@gmail.com>
2020-02-29 15:45:24 -0800

    an idea

---
+++ Sources/Playlister/Explore/ArtistIncomeCommand.swift
@@ -0,0 +1,9 @@
+//
+//  ArtistIncomeCommand.swift
+//  Files
+//
+//  Created by Grey Patterson on 2/29/20.
+//
+
+import Foundation
+// TODO: This should take an artist name, then get all their songs, and then total up the counts across all of those, and finally multiply it by Apple Music's 'payout per artist' thing
