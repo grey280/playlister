cd5a0e75878f92c2734b9d7cb4d67200b6d181ad
grey280 <greypatterson@gmail.com>
2020-02-29 15:43:39 -0800

    There that works

--- Sources/Playlister/main.swift
+++ Sources/Playlister/main.swift
@@ -1,5 +1,5 @@
 import SwiftCLI
 
 let cli = CLI(name: "playlister", version: "2.0.0", description: "A tool for tracking your iTunes/Music.app playlists as they change over time.", commands: [MarkdownCommand(), ListCommand(), GenerateCommand(), DatabaseCommandGroup(), ExploreCommandGroup()])
-cli.aliases = ["e": "explore"]
+cli.aliases["e"] = "explore"
 let _ = cli.go()
