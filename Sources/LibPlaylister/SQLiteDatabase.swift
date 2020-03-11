48320825b1f5830b601ac4ae82b0b2a0bb77a25c
grey280 <greypatterson@gmail.com>
2020-03-11 16:39:45 -0700

    changing the API surface a bit

--- Sources/LibPlaylister/SQLiteDatabase.swift
+++ Sources/LibPlaylister/SQLiteDatabase.swift
@@ -11,25 +11,31 @@ import SQLite
 
 public class SQLiteDatabase{
     let file: File
+    var database: Connection!
     
-    public init(_ with: File){
+    public init(_ with: File) throws{
         self.file = with
+        try initialize()
     }
     public init() throws {
         self.file = try Folder.home
             .createSubfolderIfNeeded(at: ".playlister")
             .createFileIfNeeded(at: "links.sqlite3")
+        try initialize()
     }
     
-    func initialize(_ deletingOld: Bool) throws {
-        if (deletingOld){
-            try file.delete()
-        }
-        let db = try Connection(file.path)
+    public func resetDatabase() throws {
+        database = nil
+        try file.delete()
+        try initialize()
+    }
+    
+    func initialize() throws {
+        database = try Connection(file.path)
         let links = Table("links")
-        let id = Expression<Int64>("id")
+        let id = Expression<Int>("id")
         let link = Expression<URL?>("link")
-        try db.run(links.create { t in
+        try database.run(links.create(ifNotExists: true) { t in
             t.column(id, primaryKey: true)
             t.column(link)
         })
