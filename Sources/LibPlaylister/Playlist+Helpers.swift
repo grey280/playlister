c21832d16179a4be0973892ace9c2725b0fe68cb
grey280 <greypatterson@gmail.com>
2020-03-11 15:10:45 -0700

    This is cleaner

--- Sources/LibPlaylister/Playlist+Helpers.swift
+++ Sources/LibPlaylister/Playlist+Helpers.swift
@@ -26,7 +26,33 @@ public extension Playlist {
         return children.compactMap { $0.findPlaylist(named: named) }.first
     }
     
-    func asMarkdown(includeLinks: Bool = false, usingLinkStore: LinkStore? = nil){
+    func asMarkdown(includeRatings: Bool = false, usingLinkStore linkStore: LinkStore? = nil) -> String? {
+        if isParent {
+            return nil
+        }
+        let body = items.map { (item) -> String in
+            item.asMarkdown(includeRatings: includeRatings, usingLinkStore: linkStore)
+        }.joined(separator: "\n\n")
+        return "# \(name.markdownSafe)\n\n\(body)\n"
+    }
+}
+
+extension PlaylistItem {
+    func asMarkdown(includeRatings: Bool, usingLinkStore linkStore: LinkStore?) -> String {
+        let artistName = artist?.name?.markdownSafe
         
+        var result = "**\(title?.markdownSafe ?? "(Untitled item)")** - \(artistName ?? "Unknown artist")"
+        if let albumTitle = album?.name?.markdownSafe {
+            result = result + " on *\(albumTitle)"
+        }
+        if (includeRatings){
+            result = result + "" // TODO: Implement
+        }
+        if let store = linkStore {
+            if let url = store.link(for: self) {
+                result = "(\(result))[\(url.absoluteString)]"
+            }
+        }
+        return result
     }
 }
