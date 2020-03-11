06f1b26e411c5e2457c791c78e5ba62ea1057130
grey280 <greypatterson@gmail.com>
2020-03-11 16:39:56 -0700

    and it works as a LinkStore!

--- Sources/LibPlaylister/SQLiteDatabase.swift
+++ Sources/LibPlaylister/SQLiteDatabase.swift
@@ -42,7 +42,6 @@ public class SQLiteDatabase{
     }
 }
 
-
 extension URL: Value {
     public static let declaredDatatype: String = "TEXT"
     public typealias Datatype = String
@@ -51,5 +50,36 @@ extension URL: Value {
     }
     public static func fromDatatypeValue(_ datatypeValue: String) -> URL {
         URL(string: datatypeValue)!
+    }
+}
+
+extension SQLiteDatabase: LinkStore{
+    public func link(for item: PlaylistItem) -> URL? {
+        guard let db = database else {
+            // TODO: Log the error in some way
+            return nil
+        }
+        let table = Table("links")
+        let id = Expression<Int>("id")
+        let link = Expression<URL?>("link")
+        if let row = try? database.prepare(table.filter(id == item.id).limit(1)).first(where: { (_) -> Bool in
+            true
+        }) {
+            return row[link]
+        } else {
+            var input: String?
+            while (input == nil){
+                print("What link would you like to use for \(item.asMarkdown(ratingFormatter: nil, usingLinkStore: nil))? (Leave blank for no link.)")
+                input = readLine(strippingNewline: true)
+                if (input == nil) {
+                    continue
+                }
+                let url: URL? = input == "" ? nil : URL(string: input!)
+                let insert = table.insert(id <- item.id, link <- url)
+                let _ = try? db.run(insert)
+                return url
+            }
+        }
+        return nil // should never actually happen but here we are
     }
 }
