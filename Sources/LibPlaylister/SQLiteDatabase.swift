412732f996ee7647f2c4cd3089a02c7de3b2b575
grey280 <greypatterson@gmail.com>
2020-03-11 16:00:06 -0700

    Initialize

--- Sources/LibPlaylister/SQLiteDatabase.swift
+++ Sources/LibPlaylister/SQLiteDatabase.swift
@@ -20,4 +20,18 @@ public struct SQLiteDatabase{
             .createSubfolderIfNeeded(at: ".playlister")
             .createFileIfNeeded(at: "links.sqlite3")
     }
+    
+    func initialize(_ deletingOld: Bool) throws {
+        if (deletingOld){
+            try file.delete()
+        }
+        let db = try Connection(file.path)
+        let links = Table("links")
+        let id = Expression<Int64>("id")
+        let link = Expression<String?>("link")
+        try db.run(links.create { t in
+            t.column(id, primaryKey: true)
+            t.column(link)
+        })
+    }
 }
