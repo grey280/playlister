6a4e7754743379194f3e57b859c53dffe1a2c0a1
grey280 <greypatterson@gmail.com>
2020-03-11 17:06:53 -0700

    Well that seems like it might work

--- Sources/Playlister/main.swift
+++ Sources/Playlister/main.swift
@@ -1,6 +1,19 @@
-import SwiftCLI
+//import SwiftCLI
+//
+//let cli = CLI(name: "playlister", version: "2.0.0", description: "A tool for tracking your iTunes/Music.app playlists as they change over time.", commands: [MarkdownCommand(), ListCommand(), GenerateCommand(), ExploreCommandGroup()])
+//cli.aliases["e"] = "explore"
+//cli.aliases["db"] = "database"
+//let _ = cli.go()
+//
+//
 
-let cli = CLI(name: "playlister", version: "2.0.0", description: "A tool for tracking your iTunes/Music.app playlists as they change over time.", commands: [MarkdownCommand(), ListCommand(), GenerateCommand(), DatabaseCommandGroup(), ExploreCommandGroup()])
-cli.aliases["e"] = "explore"
-cli.aliases["db"] = "database"
-let _ = cli.go()
+import ArgumentParser
+
+struct CLI: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "playlister",
+        abstract: "A command-line tool for reading your iTunes/Music.app playlists",
+        subcommands: [Database.self]
+    )
+}
+CLI.main()
