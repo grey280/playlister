5d21523f147fedf8bc39bb114b2f8ac65e6b2d6b
grey280 <greypatterson@gmail.com>
2020-03-14 11:33:56 -0700

    Command!

--- Sources/Playlister/main.swift
+++ Sources/Playlister/main.swift
@@ -13,7 +13,7 @@ struct CLI: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "playlister",
         abstract: "A command-line tool for reading your iTunes/Music.app playlists",
-        subcommands: [Database.self, Explore.self, List.self]
+        subcommands: [Database.self, Explore.self, List.self, Markdown.self]
     )
 }
 CLI.main()
