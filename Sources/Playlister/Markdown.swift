ed4d2aa0006b895d5889c94eb786cce2919fa6d9
grey280 <greypatterson@gmail.com>
2020-03-14 11:29:59 -0700

    Run it!

--- Sources/Playlister/Markdown.swift
+++ Sources/Playlister/Markdown.swift
@@ -7,6 +7,7 @@
 
 import LibPlaylister
 import ArgumentParser
+import Files
 
 struct Markdown: ParsableCommand {
     static var configuration: CommandConfiguration = CommandConfiguration(commandName: "md", abstract: "Generate a Markdown file of a playlist")
@@ -19,7 +20,15 @@ struct Markdown: ParsableCommand {
     func run() throws {
         let library = try MusicLibrary()
         guard let list = library.findPlaylist(named: playlistName) else {
-            throw RuntimeError(description: "Playlist not found.")
+            throw RuntimeError("Playlist not found.")
         }
+        let database = includeLinks ? try SQLiteDatabase() : nil
+        let folder = Folder.current
+        let file = try folder.createFile(named: playlistName + ".md")
+        let formatter = includeRatings ? FiveStarRatingFormatter() : nil
+        guard let body = list.asMarkdown(ratingFormatter: formatter, usingLinkStore: database) else {
+            throw RuntimeError("Unable to generate playlist body.")
+        }
+        try file.write(body)
     }
 }
