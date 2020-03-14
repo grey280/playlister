f75b7b82be6322c2a3144b1ebd85d38a2192f201
grey280 <greypatterson@gmail.com>
2020-03-14 11:52:28 -0700

    okay ShellOut is neat

--- Sources/Playlister/Generate.swift
+++ Sources/Playlister/Generate.swift
@@ -5,4 +5,28 @@
 //  Created by Grey Patterson on 3/14/20.
 //
 
-import Foundation
+import LibPlaylister
+import ArgumentParser
+import ShellOut
+import Files
+
+struct Generate: ParsableCommand {
+    static var configuration: CommandConfiguration = CommandConfiguration(commandName: "generate", abstract: "Generate all playlists as markdown.", discussion: "Creates a full hierarchy of all your (normal, so excluding things like Purchased) playlists, as markdown files.\nAll generation happens in a ./playlists directory.\nAfter generation, will optionally attempt to add, commit, and push changes to the ./playlists directory as a git repository.")
+    
+    @Flag(name: [.long, .customShort("r")]) var includeRatings: Bool
+    @Flag(name: [.long, .customShort("l")], help: "Non-interactively include links. To fill the links database, use the 'md' command.") var includeLinks: Bool
+    @Flag(name: [.long, .customShort("g")]) var git: Bool
+    
+    func run() throws {
+        let library = try MusicLibrary()
+        let rootFolder = try Folder.current.createSubfolderIfNeeded(at: "playlists")
+        if (git){
+            try shellOut(to: ["git", "init"], at: rootFolder.path)
+            try shellOut(to: ["git", "fetch"], at: rootFolder.path)
+            try shellOut(to: ["git", "pull"], at: rootFolder.path)
+            try shellOut(to: ["git", "rm", "-rf", "."], at: rootFolder.path)
+            try shellOut(to: ["git", "clean", "-fxd"], at: rootFolder.path)
+        }
+       
+    }
+}
