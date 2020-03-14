21007640271a0861b248c7d37d8b17e8a1dcf1e2
grey280 <greypatterson@gmail.com>
2020-03-14 11:40:37 -0700

    one left!

--- Sources/Playlister/MarkdownCommand.swift
+++ Sources/Playlister/MarkdownCommand.swift
@@ -4,66 +4,6 @@ import Files
 import SQLite
 import LibPlaylister
 
-class MarkdownCommand: Command {
-    let name = "md"
-    let shortDescription: String = "Generate a Markdown file of a playlist"
-    
-    @Param var playlistName: String?
-    
-    @Flag("-r", "--ratings", description: "Include star ratings") var includeRatings: Bool
-    @Flag("-l", "--links", description: "Include links") var includeLinks: Bool
-    
-    @Key("-o", "--output", description: "Output directory; defaults to current directory") var outputPath: String?
-    
-    @Key("-n", "--name", description: "Output file name; defaults to the name of the selected playlist") var outputName: String?
-    
-    func execute() throws {
-        let library = try _Library()
-        let listName: String
-        if playlistName == nil {
-            for playlist in library.playlists{
-                printList(playlist, depth: 0)
-            }
-            listName = Input.readLine(prompt: "Which playlist?")
-        } else {
-            listName = playlistName!
-        }
-        
-        guard let list = library.findPlaylist(named: listName) else {
-            stderr <<< "Playlist not found."
-            return
-        }
-        
-        var db: Connection? = nil
-        if includeLinks {
-            let workDir = try Folder.home.createSubfolderIfNeeded(at: ".playlister")
-            guard let dbFile = try? workDir.file(at: "links.sqlite3") else {
-                stderr <<< "Database not initialized. Run `playlister db init` first."
-                return
-            }
-            db = try Connection(dbFile.path)
-        }
-        
-        let folder: Folder
-        if let outPath = outputPath {
-            folder = try Folder(path: outPath)
-        } else {
-            folder = .current
-        }
-        let file = try folder.createFile(named: (outputName ?? listName) + ".md")
-        
-        try file.write(list.asMarkdown(includeRating: includeRatings, linkDatabase: db) ?? "Unable to parse")
-    }
-    
-    
-    func printList(_ list: _Playlist, depth: Int){
-        let result = "\(String(repeating: " ", count: depth)) \(list.name)"
-        print(result)
-        for playlist in list.children {
-            printList(playlist, depth: depth + 1)
-        }
-    }
-}
 
 internal extension _Playlist {
     /// Convert a playlist to markdown
--- Sources/Playlister/main.swift
+++ Sources/Playlister/main.swift
@@ -1,11 +1,7 @@
 //import SwiftCLI
 //
-//let cli = CLI(name: "playlister", version: "2.0.0", description: "A tool for tracking your iTunes/Music.app playlists as they change over time.", commands: [MarkdownCommand(), ListCommand(), GenerateCommand()])
-//cli.aliases["e"] = "explore"
-//cli.aliases["db"] = "database"
+//let cli = CLI(name: "playlister", version: "2.0.0", description: "A tool for tracking your iTunes/Music.app playlists as they change over time.", commands: [GenerateCommand()])
 //let _ = cli.go()
-//
-//
 
 import ArgumentParser
 
