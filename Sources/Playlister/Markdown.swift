0b923d1b967d0d0aa15df8be44d253452af96e1e
grey280 <greypatterson@gmail.com>
2020-03-14 11:23:34 -0700

    start on the structure

--- Sources/Playlister/Markdown.swift
+++ Sources/Playlister/Markdown.swift
@@ -5,4 +5,21 @@
 //  Created by Grey Patterson on 3/14/20.
 //
 
-import Foundation
+import LibPlaylister
+import ArgumentParser
+
+struct Markdown: ParsableCommand {
+    static var configuration: CommandConfiguration = CommandConfiguration(commandName: "md", abstract: "Generate a Markdown file of a playlist")
+    
+    @Argument() var playlistName: String
+    
+    @Flag() var includeRatings: Bool
+    @Flag() var includeLinks: Bool
+    
+    func run() throws {
+        let library = try MusicLibrary()
+        guard let list = library.findPlaylist else {
+            
+        }
+    }
+}
