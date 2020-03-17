ed1ed69e7b79208ff6b89d431397373511c01b44
grey280 <greypatterson@gmail.com>
2020-03-16 20:09:38 -0700

    More cleanup

--- Sources/LibPlaylister/Helpers.swift
+++ Sources/LibPlaylister/Helpers.swift
@@ -7,21 +7,6 @@
 
 import Foundation
 
-func printedRating(_ rating: Int) -> String {
-    var rate = rating
-    if rate < 0 {
-        rate = 0
-    }
-    rate = rate / 20
-    var unRate = 5 - rate
-    if unRate < 0 {
-        unRate = 0
-    }
-    let yes = String(repeating: "★", count: rate)
-    let no = String(repeating: "☆", count: unRate)
-    return yes + no
-}
-
 extension String{
     public var markdownSafe: String{
         return self.replacingOccurrences(of: "*", with: "\\*").replacingOccurrences(of: "[", with: "\\[").replacingOccurrences(of: "]", with: "\\]")
