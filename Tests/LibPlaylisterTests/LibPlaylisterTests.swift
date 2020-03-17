bbe37576a680684926e38d9e73429712f6c0a6ef
grey280 <greypatterson@gmail.com>
2020-03-16 20:07:27 -0700

    my first test!

--- Tests/LibPlaylisterTests/LibPlaylisterTests.swift
+++ Tests/LibPlaylisterTests/LibPlaylisterTests.swift
@@ -1,7 +1,19 @@
 import XCTest
+import LibPlaylister
 import class Foundation.Bundle
 
 final class LibPlaylisterTests: XCTestCase {
+    func testMarkdownSafe() throws {
+        let string1 = "F**k Collingwood"
+        let string1safe = #"F\*\*k Collingwood"#
+        XCTAssertEqual(string1safe, string1.markdownSafe)
+        let string2 = "Nothing problematic here"
+        XCTAssertEqual(string2, string2.markdownSafe)
+        let string3 = "This has [an insertion]"
+        let string3safe = #"This has \[an insertion\]"#
+        XCTAssertEqual(string3safe, string3.markdownSafe)
+    }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
