0425e63e14fbb963c5b09003d5d9120830959016
grey280 <greypatterson@gmail.com>
2020-03-16 20:02:10 -0700

    rename

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
             dependencies: ["Files", "SQLite"]
         ),
         .testTarget(
-            name: "PlaylisterTests",
+            name: "LibPlaylisterTests",
             dependencies: ["LibPlaylister"]),
     ]
 )
