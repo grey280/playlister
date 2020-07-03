62ad99c7299b2ce380faad2a30fc6cfcc01fa292
grey280 <greypatterson@gmail.com>
2020-07-03 11:14:46 -0700

    Create update command

--- Sources/Playlister/Database.swift
+++ Sources/Playlister/Database.swift
@@ -13,7 +13,7 @@ struct Database: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "db",
         abstract: "Manage the links database",
-        subcommands: [DatabaseReset.self]
+        subcommands: [DatabaseReset.self, DatabaseUpdate.self]
     )
 }
 
@@ -26,5 +26,33 @@ struct DatabaseReset: ParsableCommand {
     func run() throws {
         let database = try SQLiteDatabase()
         try database.resetDatabase()
+    }
+}
+
+struct DatabaseUpdate: ParsableCommand {
+    static var configuration: CommandConfiguration = CommandConfiguration(
+        commandName: "update",
+        abstract: "Update the link for a track"
+    )
+    
+    @Argument() var songID: Int
+    @Argument() var url: String
+    
+    func run() throws {
+        #if os(macOS)
+        guard let url = URL(string: url) else {
+            throw RuntimeError("Could not parse URL")
+        }
+        let library = try MusicLibrary()
+        guard let item = library.items.filter({ (item) -> Bool in
+            item.id == songID
+        }).first else {
+            throw RuntimeError("Track not found")
+        }
+        let database = try SQLiteDatabase(interactive: false)
+        try database.link(for: item, url: url)
+        #else
+        throw RuntimeError("Unsupported operating system!")
+        #endif
     }
 }
